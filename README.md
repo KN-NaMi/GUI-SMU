fc2b23a97c02e5b0ffc2e56e6b12d037da9dda2f
Jakub Naumowicz <jakub.oliwer@gmail.com>
2025-03-12 01:54:46 +0100

    in-prod-2

--- README.md
+++ README.md
@@ -1,54 +1,44 @@
-# React + TypeScript + Vite
+# GUI - SMU: KEITHLEY 2401
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+Interface do obsługi, odczytu oraz zapisu pomiarów z jednostki SMU.
 
-Currently, two official plugins are available:
+## Narzędzia deweloperskie / technologie
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
-
-## Expanding the ESLint configuration
-
-If you are developing a production application, we recommend updating the configuration to enable type-aware lint rules:
-
-```js
-export default tseslint.config({
-  extends: [
-    // Remove ...tseslint.configs.recommended and replace with this
-    ...tseslint.configs.recommendedTypeChecked,
-    // Alternatively, use this for stricter rules
-    ...tseslint.configs.strictTypeChecked,
-    // Optionally, add this for stylistic rules
-    ...tseslint.configs.stylisticTypeChecked,
-  ],
-  languageOptions: {
-    // other options...
-    parserOptions: {
-      project: ['./tsconfig.node.json', './tsconfig.app.json'],
-      tsconfigRootDir: import.meta.dirname,
-    },
-  },
-})
+**Narzędzia potrzebe do rozwijania frontendu:**
+- Dla uproszczenia pracy, dobrze korzystać z terminala **Git Bash**, <ins>zamiast powershella<ins/>
+- [NodeJS](https://nodejs.org/en)
+    - dobrze pobrać też narzędzie [npm-run-all](https://www.npmjs.com/package/npm-run-all)
+- [TypeScript](https://www.npmjs.com/package/typescript)
+- electron-builder
+```
+npm i --save-dev electron-builder
 ```
 
-You can also install [eslint-plugin-react-x](https://github.com/Rel1cx/eslint-react/tree/main/packages/plugins/eslint-plugin-react-x) and [eslint-plugin-react-dom](https://github.com/Rel1cx/eslint-react/tree/main/packages/plugins/eslint-plugin-react-dom) for React-specific lint rules:
+**Technologie:**
+- Electron
+- Vite
+- React
 
-```js
-// eslint.config.js
-import reactX from 'eslint-plugin-react-x'
-import reactDom from 'eslint-plugin-react-dom'
+## Praca przy aplikacji
 
-export default tseslint.config({
-  plugins: {
-    // Add the react-x and react-dom plugins
-    'react-x': reactX,
-    'react-dom': reactDom,
-  },
-  rules: {
-    // other rules...
-    // Enable its recommended typescript rules
-    ...reactX.configs['recommended-typescript'].rules,
-    ...reactDom.configs.recommended.rules,
-  },
-})
+1. **Build**
+```
+npm run build
+```
+2. **Otworzenie aplikacji w trybie deweloperskim** (skonfigurowany jest local server Vite, więc zmiany wprowadzane w projekcie widoczne są od razu w trybie deweloperskim, bez konieczności odświeżania)
+```
+npm run dev
+```
+3. **Budowanie gotowej aplikacji** (na windowsie potrzebne będzie otworzenie Git Basha, lub w przypadku korzystania z konsoli w edytorze, np. VS Code samego edytora, w trybie administratora)
+- Dla Windowsa
+```
+npm run dist:win
+```
+- Dla Mac
+```
+npm run dist:mac
+```
+- Dla Linuxa
+```
+npm run dist:linux
 ```
--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,22 +1,39 @@
 import './App.css'
-import React from "react";
+import React, { useState } from "react";
 
 interface ToolbarProps {}
 
 const Toolbar: React.FC<ToolbarProps> = () => {
+
+  const [sourceType, setSourceType] = useState<string>("voltage-src");
+
   return (
     <div className='toolbar'>
+      
+      <div className='first-column'>
+        <div className='start-stop-buttons'>
+          {/* przyciski start i stop */}
+          <button className='start-btn'>
+            <img src="/play.png" alt="play-icon" />
+          </button>
 
-      <div className='start-stop-buttons'>
+          <button className='stop-btn'>
+            <img src="/stop.png" alt="stop-icon" />
+          </button>
 
-        <button className='start-btn'>
-          <img src="/play.png" alt="play-icon" />
-        </button>
+        </div>
 
-        <button className='stop-btn'>
-          <img src="/stop.png" alt="stop-icon" />
-        </button>
-
+        <div className='label-select-corelation'>
+          <label htmlFor="current-voltage-src">Typ źródła</label>
+          <select 
+            id="current-voltage-src"
+            value={sourceType}
+            onChange={(e) => setSourceType(e.target.value)}
+          >
+            <option value="voltage-src">Źródło napięciowe</option>
+            <option value="current-src">Źródło prądowe</option>
+          </select>
+        </div>
       </div>
 
       <div className='COM-select'>
@@ -36,36 +53,66 @@ const Toolbar: React.FC<ToolbarProps> = () => {
       </div>
 
       <div className='input-container'>
-        <div className='input-label-corelation'>
-          <label htmlFor="current-limiter">Ograniczenie prądowe</label>
-          <div>
-            <input type="text" id='current-limiter' />
-            <select name="units" id="current-limiter-units">
-              <option value="μA">μA</option>
-              <option value="mA">mA</option>
-              <option value="A">A</option>
-              <option value="kA">kA</option>
-            </select>
-          </div>
-        </div>
+        {sourceType === "voltage-src" && (
+          <>
+            {/* Źródło napięciowe */}
+            <div className='input-label-corelation nw'>
+              <label htmlFor="current-limiter">Ograniczenie prądowe</label>
+              <div>
+                <input type="text" id='current-limiter' />
+                <select name="units" id="current-limiter-units">
+                  <option value="μA">μA</option>
+                  <option value="mA">mA</option>
+                  <option value="A">A</option>
+                  <option value="kA">kA</option>
+                </select>
+              </div>
+            </div>
 
-        <div className='input-label-corelation'>
-          <label htmlFor="U-max">U_max</label>
-          <input type="text" id='U-max'/>
-        </div>
-        
-        <div className='input-label-corelation'>
-          <label htmlFor="how-many-measurements">Ilość punktów pomiarowych</label>
-          <input type="text" id='how-many-measurements'/>
-        </div>
+            <div className='input-label-corelation ne'>
+              <label htmlFor="U-max">U_max</label>
+              <input type="text" id='U-max'/>
+            </div>
+            
+            <div className='input-label-corelation se'>
+              <label htmlFor="U-min">U_min</label>
+              <input type="text" id='U-min'/>
+            </div>
+          </>
+        )}
+        {sourceType === "current-src" && (
+          <>
+            {/* Źródło prądowe */}
+            <div className='input-label-corelation nw'>
+              <label htmlFor="voltage-limiter">Ograniczenie napięciowe</label>
+              <div>
+                <input type="text" id='voltage-limiter' />
+                <select name="units" id="voltage-limiter-units">
+                  <option value="μV">μV</option>
+                  <option value="mV">mV</option>
+                  <option value="V">V</option>
+                  <option value="kV">kV</option>
+                </select>
+              </div>
+            </div>
 
-        <div className='input-label-corelation'>
-          <label htmlFor="U-min">U_min</label>
-          <input type="text" id='U-min'/>
-        </div>
+            <div className='input-label-corelation ne'>
+              <label htmlFor="I-max">I_max</label>
+              <input type="text" id='I-max'/>
+            </div>
+
+            <div className='input-label-corelation se'>
+              <label htmlFor="I-min">I_min</label>
+              <input type="text" id='I-min'/>
+            </div>
+          </>
+        )}
+            <div className='input-label-corelation sw'>
+              <label htmlFor="how-many-measurements">Ilość punktów pomiarowych</label>
+              <input type="text" id='how-many-measurements'/>
+            </div>
         
       </div>
-
     </div>
   );
 };
