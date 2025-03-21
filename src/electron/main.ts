8b7efc05ade51a78b6cf2234b54026f1eedeec0a
Jakub Naumowicz <jakub.oliwer@gmail.com>
2025-03-21 14:27:49 +0100

    commented-chart-base

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,16 +1,20 @@
 import { app, BrowserWindow } from 'electron';
 import path from 'path';
 import { isDev } from './util.js';
+import { getPreloadPath } from './pathResolver.js';
 
 app.on("ready", ()=>{
     const mainWindow = new BrowserWindow({
-        width: 800,
-        height: 600,
+        width: 1920,
+        height: 1080,
         icon: path.join(app.getAppPath(), 'public', process.platform === 'win32' ? 'icon.ico' : 'icon.ico'),
         webPreferences: {
-            nodeIntegration: true,
+            preload: getPreloadPath(),
         },
     });
+
+    mainWindow.maximize();
+
     if (isDev()) {
         mainWindow.loadURL('http://localhost:5123');
     } else {
