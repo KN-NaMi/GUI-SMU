d12f8e276a2590919b6628d1d02697d6de2926fd
Kacper <kszafranski26@gmail.com>
2025-07-10 10:50:14 +0200

    4w configuration test

--- backend/main.py
+++ backend/main.py
@@ -170,6 +170,8 @@ class MeasureProcedure(Procedure):
                 is_4_wire=self.is_4_wire
             )
             
+            print(f"Current wire configuration: {self.meter.instrument.wires}")
+            
             self.voltages = self._generate_sweep_array(
                 self.voltage_start, self.voltage_end, self.iterations, self.is_both_ways
             )
@@ -186,6 +188,8 @@ class MeasureProcedure(Procedure):
                 compliance_voltage=self.compliance_voltage,
                 is_4_wire=self.is_4_wire
             )
+            
+            print(f"Current wire configuration: {self.meter.instrument.wires}")
             
             self.currents = self._generate_sweep_array(
                 self.current_start, self.current_end, self.iterations, self.is_both_ways
